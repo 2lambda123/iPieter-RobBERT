070ead2b0d1f0c523c5dfcc7ea795c1f74862894
Restyled.io <commits@restyled.io>
2024-01-20 05:27:39 +0000

    Restyled by autopep8

--- examples/die_vs_data_rest_api/app.py
+++ examples/die_vs_data_rest_api/app.py
@@ -1,6 +1,7 @@
 from app import create_app
 import argparse
 
+
 def create_parser():
     "Utility function to create the CLI argument parser"
 
@@ -8,14 +9,16 @@ def create_parser():
         description="Create a REST endpoint for for 'die' vs 'dat' disambiguation."
     )
 
-    parser.add_argument("--model-path", help="Path to the finetuned RobBERT identifier.", required=False)
-    parser.add_argument("--fast-model-path", help="Path to the mlm RobBERT identifier.", required=False)
+    parser.add_argument(
+        "--model-path", help="Path to the finetuned RobBERT identifier.", required=False)
+    parser.add_argument(
+        "--fast-model-path", help="Path to the mlm RobBERT identifier.", required=False)
 
     return parser
+
 
 if __name__ == "__main__":
     arg_parser = create_parser()
     args = arg_parser.parse_args()
-
 
     create_app(args.model_path, args.fast_model_path).run()
